529b0610bd7fc964052e5e3976d3b3840dfa1e43
saguaro1234 <165747831+saguaro1234@users.noreply.github.com>
2025-05-19 13:31:39 -0700

    api key

--- petfinder_microservice.py
+++ petfinder_microservice.py
@@ -1,6 +1,15 @@
 import requests
+import socket
+import json
+
+
+HOST = '127.0.0.1'
+PORT = 5555
 CLIENT_ID = "60XhvkrXIhsaNBOQSkaTiPMw2lYOVngKLcnpOcRj4E3OkX2N3m"
 CLIENT_SECRET = "aX9Yb1AOTTwZhwibuJgY5A91RgTTaWIw0UXSpzxo"
+
+
+
 
 def get_token():
     url = "https://api.petfinder.com/v2/oauth2/token"
@@ -10,24 +19,40 @@ def get_token():
     response = requests.post(url, data)
     response.raise_for_status()
     return response.json()["access_token"]
+
+
 def get_organizations(zip_code, access_token):
     url = "https://api.petfinder.com/v2/organizations"
     response = requests.get(url, params={"location": zip_code, "distance": 50, "limit": 10}, headers={"Authorization": f"Bearer {access_token}"})
     return response.json()["organizations"]
 
+
 def main():
-    zip_code = input("Please provide a zip code: ")
-    token = get_token()
-    orgs = get_organizations(zip_code, token)
-    for item in orgs:
-        name = item["name"]
-        address = item["address"]
-        email = item["email"]
-        phone = item["phone"]
-
-
-        print(f"{name}, ({address["city"]}, {address["state"]}), Email: {email}, Phone: {phone}")
-
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            data = conn.recv(4096).decode('utf-8')
+            if not data:
+                return
+            json_data = json.loads(data)
+            zip_code = json_data.get("zipcode", "")
+            token = get_token()
+            orgs = get_organizations(zip_code, token)
+            response = {}
+            for item in orgs:
+                org = {}
+                name = item["name"]
+                address = item["address"]
+                email = item["email"]
+                phone = item["phone"]
+                org["name"] = name
+                org["email"] = email
+                org["phone"] = phone
+                org["address"] = (address["city"], address["state"])
+                response[name] = org
+            conn.sendall(json.dumps(response).encode('utf-8'))
 
 if __name__ == "__main__":
     main()
